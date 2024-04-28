7e8273bc55ed4e96f1d661332ce31d93eb2fd41c
gautam <gautam.k.viswanathan@gmail.com>
2024-04-29 01:53:11 +0530

    deploy changes

--- yocket_project/backend/server.js
+++ yocket_project/backend/server.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const app = express();
-
+app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://tourmaline-faun-ea35e6.netlify.app/"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
