0bdf5b180da15d5cce0267f00030ae226d1c95a6
gautam <gautam.k.viswanathan@gmail.com>
2024-04-29 01:55:30 +0530

    deploy changes

--- yocket_project/backend/server.js
+++ yocket_project/backend/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+var cors = require("cors");
 const app = express();
+
 app.use(cors());
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
